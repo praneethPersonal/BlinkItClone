7e388ebf6522a027cc1f08b286c5ed703215a964
Praneeth2202 <77240526+hahaha225@users.noreply.github.com>
2024-12-31 00:28:15 +0530

    Add files via upload

---
+++ BlinkItClone/BlinkItClone/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BlinkItClone.Model
+{
+    public class Category
+    {
+        public string _id { get; set; }
+        public string category_name { get; set; }
+        public string description { get; set; }
+    }
+}
